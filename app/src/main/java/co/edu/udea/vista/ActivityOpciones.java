c472e9773f12121be106871f751ac63f497e9c2c
LAURA VELEZ <landrea.velez@gmail.com>
2016-06-05 11:56:51 -0500

    Cambios Perfil de usuario

--- app/src/main/java/co/edu/udea/vista/ActivityOpciones.java
+++ app/src/main/java/co/edu/udea/vista/ActivityOpciones.java
@@ -83,6 +83,8 @@ public class ActivityOpciones extends Activity implements View.OnClickListener{
         }
         else if(view.getId() == miInformacionBtn.getId())
         {
+            Intent i = new Intent(this, ActivityPerfil.class );
+            startActivity(i);
 
         }
     }
